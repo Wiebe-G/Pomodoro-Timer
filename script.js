8014023da6be4c9728e3aafc877d4cdce1e12d08
Wiebe Gouma <goumawiebe1234@proton.me>
2025-07-15 12:14:48 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+const startKnop = document.getElementById('startKnop');
+const stopKnop = document.getElementById('stopKnop');
+const timerPlek = document.getElementById('timer');
+const statusP = document.getElementById('status');
+const Focus = document.getElementById('focus');
+const Pauze = document.getElementById('pauze');
+let intervalID;
+let isRunning = false;
+
+
+startKnop.addEventListener('click', () => {
+    if (isRunning) return; // voorkomen van functie meerdere keren uitvoeren
+    isRunning = true;
+    startTimer()
+})
+
+stopKnop.addEventListener('click', () => {
+    isRunning = false;
+    stopTimer()
+})
+
+function startTimer() {
+    intervalID = setInterval(updateTimer, 1000)
+};
+
+function stopTimer() {
+    clearInterval(intervalID);
+};
+
+
+let timerSeconden;// = 1;
+
+function berekenTimerSeconden() {
+    if (Focus.classList.contains('fatHeader')) {
+        timerSeconden = 5//25 * 60;
+    } else {
+        timerSeconden = 3//300;
+    }
+    updateTimer()
+    return timerSeconden
+}
+
+berekenTimerSeconden()
+
+function updateTimer() {
+    // bereken minuten, seconden, en tijd formatteren
+    const minuten = Math.floor(timerSeconden / 60);
+    const seconden = timerSeconden % 60;
+    // minder dan 10 seconden, voeg een nul toe aan het begin
+    const geformateerdeTijd = `${minuten}:${seconden < 10 ? '0' : ''}${seconden}`;
+    timerPlek.textContent = geformateerdeTijd;
+    // 1 seconde minder
+    timerSeconden--;
+    if (timerSeconden < 0) {
+        stopTimer()
+        statusP.textContent = "Timer is voorbij"
+        Focus.classList.toggle('fatHeader');
+        Pauze.classList.toggle('fatHeader');
+        berekenTimerSeconden()
+        isRunning = false;
+    }
+}
+
+
+Focus.addEventListener('click', () => {
+    Pauze.classList.remove('fatHeader');
+    Focus.classList.add('fatHeader');
+    berekenTimerSeconden()
+});
+
+Pauze.addEventListener('click', () => {
+    Focus.classList.remove('fatHeader');
+    Pauze.classList.add('fatHeader');
+    berekenTimerSeconden()
+});
